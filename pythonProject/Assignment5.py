e218a1ab93548f0d62b4670ba2a91f7e821fc858
MinhasInder <48483688+MinhasInder@users.noreply.github.com>
2022-02-17 18:02:57 -0500

    Assignment5

---
+++ pythonProject/Assignment5.py
@@ -0,0 +1,65 @@
+
+# Name : Inderpreet Singh Minhas
+# Student Id: 400430266
+
+class PasswordManagementSystem:
+    #Answer 1
+    def get_login_info_entered_by_user_on_website(self):
+        return None
+
+    def check_if_login_info_has_password(self):
+        return None
+
+    def check_if_login_info_has_username(self):
+        return None
+
+    def check_if_password_is_encrypted(self):
+        return None
+
+    def check_if_username_is_encrypted(self):
+        return None
+
+    def encrypt_password(self):
+        return None
+
+    def decrypt_username(self):
+        return None
+
+    def save_login_info(self):
+        return None
+
+    #Answer 2
+
+    def get_website_being_visited(self):
+        return None
+
+    def retrieve_login_information_for_website_based_on_user(self):
+        return None
+
+    def check_if_password_is_being_entered_in_login_form(self):
+        return None
+
+    def decrypt_password(self):
+        return None
+
+    #Answer 3
+
+    def check_if_login_info_being_returned_is_of_user_requesting_it(self):
+        return None
+
+    #Answer 4
+
+    def show_auto_fill_login_info_confirmation_popup(self):
+        return None
+
+    def deny_auto_fill_login_info_access(self):
+        return None
+
+    def confirm_auto_fill_login_info_access(self):
+        return None
+
+    def save_auto_fill_login_info_for_website_in_the_browser(self):
+        return None
+
+
+    
